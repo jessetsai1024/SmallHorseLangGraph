31c6e02a6ba9fc538895e39f68a8c7f7be1d5a20
Jesse <jesse1024@gmail.com>
2025-04-05 22:26:42 +0800

    05

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
-from typing import TypedDict
+import random
+from typing import List, TypedDict, Optional, Annotated, Dict, Literal
+from langgraph.checkpoint.memory import MemorySaver
 from langgraph.graph import StateGraph, START, END
+from common import *
+from dotenv import load_dotenv
+
+print("*" * 100)
+start_time = time.time()  # 取得開始時間
+load_dotenv()
 
 class GraphState(TypedDict):
     """
@@ -12,16 +20,19 @@ action2_name = "(action2)"
 action3_name = "(action3)"
 
 def action1(state):
-    print(action1_name, state)
+    print(">", action1_name, state)
     return {"state": f"{action1_name} ok"}
 
 def action2(state):
-    print(action2_name, state)
+    print(">", action2_name, state)
     return {"state": f"{action2_name} ok"}
 
 def action3(state):
-    print(action3_name, state)
+    print(">", action3_name, state)
     return {"state": f"{action3_name} ok"}
+
+def condition_function(state) -> Literal["0", "1"]:
+    return str(random.choice([0, 1]))
 
 graph = StateGraph(GraphState)
 
@@ -30,12 +41,15 @@ graph.add_node(action2_name, action2)
 graph.add_node(action3_name, action3)
 
 graph.add_edge(START, action1_name)
-graph.add_edge(action1_name, action2_name)
-graph.add_edge(action2_name, action3_name)
+graph.add_conditional_edges(
+    action1_name, condition_function, {"0": action2_name, "1": action3_name}
+)
+graph.add_edge(action2_name, END)
 graph.add_edge(action3_name, END)
 
 app = graph.compile()
 app.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
-result = app.invoke({"state": "init"})
-print("result", result)
+# 只返回結果
+result = app.invoke({"state": "initial"})
+print(result)
