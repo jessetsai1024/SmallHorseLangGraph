3cd8f779514b65815f160a1c72cf8c0af22b11b6
Jesse <jesse1024@gmail.com>
2025-04-06 13:39:26 +0800

    07

--- main.py
+++ main.py
@@ -7,51 +7,52 @@ from common import *
 from dotenv import load_dotenv
 
 print("=" * 100)
-start_time = time.time()   # 獲取開始的時間
+start_time = time.time()   # 獲取開始時間
 load_dotenv()
 
 class GraphState(TypedDict):
     """
     Represents the state of our graph.
     """
-    state: str
-    history: Annotated[List[str], add]
+    state: Annotated[List, add]
 
 action1_name = "(action1)"
 action2_name = "(action2)"
 action3_name = "(action3)"
+action4_name = "(action4)"
 
 def action1(state):
     print(">", action1_name, state)
-    return {"state": f"{action1_name} ok", "history": [action1_name]}
+    return {"state": [f"{action1_name} ok"]}
 
 def action2(state):
     print(">", action2_name, state)
-    return {"state": f"{action2_name} ok", "history": [action2_name]}
+    return {"state": [f"{action2_name} ok"]}
 
 def action3(state):
     print(">", action3_name, state)
-    return {"state": f"{action3_name} ok", "history": [action3_name]}
+    return {"state": [f"{action3_name} ok"]}
 
-def condition_function(state) -> Literal["0", "1"]:
-    return str(random.choice([0, 1]))
+def action4(state):
+    print(">", action4_name, state)
+    return {"state": [f"{action4_name} ok"]}
 
 graph = StateGraph(GraphState)
 
 graph.add_node(action1_name, action1)
 graph.add_node(action2_name, action2)
 graph.add_node(action3_name, action3)
+graph.add_node(action4_name, action4)
 
 graph.add_edge(START, action1_name)
-graph.add_conditional_edges(
-    action1_name, condition_function, {"0": action2_name, "1": action3_name}
-)
-graph.add_edge(action2_name, END)
-graph.add_edge(action3_name, END)
+graph.add_edge(action1_name, action2_name)
+graph.add_edge(action1_name, action3_name)
+graph.add_edge([action2_name, action3_name], action4_name)
+graph.add_edge(action4_name, END)
 
 app = graph.compile()
-app.get_graph().draw_mermaid_png(output_file_path="graph.png")
+# app.get_graph().draw_mermaid_png(output_file_path="graph.png")
 
 # 只返回結果
-result = app.invoke({"state": "initial"})
+result = app.invoke({"state": ["initial"]})
 print(result)
